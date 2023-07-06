8a729b65dc1994091d5b6d64abf9335e5bbbfb4e
Utku Cengiz <cekicengiz01@gmail.com>
2023-07-06 03:27:04 +0300

    tailwind draft

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { AiOutlineDelete, AiOutlineCheck } from "react-icons/ai";
 
 function App() {
   const [newTask, setNewTask] = useState("");
@@ -36,34 +37,65 @@ function App() {
   }
 
   return (
-    <div className="app">
-      {/* Title */}
-      <h1>Tasks To-Do</h1>
+    <div className="w-screen h-screen relative flex justify-center items-center">
+      <div className="absolute">
+        {/* Title */}
+        <h1 className="text-4xl font-bold mb-10">Todo List</h1>
 
-      {/* Entering new tasks */}
-      <input
-        type="text"
-        placeholder="Enter a new task!"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        className="border-2 border-black"
-      />
+        {/* Entering new tasks */}
+        <div className="inline-flex relative items-center mb-8">
+          <input
+            type="text"
+            placeholder="Enter a new task!"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="border-2 rounded-2xl border-black py-2 px-4 text-xl font-bold"
+          />
 
-      {/* button for adding tasks */}
-      <button onClick={() => addTask()}>Add</button>
+          {/* button for adding tasks */}
+          <button
+            onClick={() => addTask()}
+            className="absolute text-xl right-4 pl-2 border-l-2 border-black h-full rounded-2xl"
+          >
+            Add
+          </button>
+        </div>
 
-      {/* lists of the entered tasks */}
-      <ul>
-        {tasks.map((task) => {
-          return (
-            <li key={task.id}>
-              <h2 className={task.completed ? "bg-black" : "bg-white"}>{task.value}</h2>
-              <button onClick={() => deleteTask(task.id)}>DELETE</button>
-              <button onClick={() => completedTasks(task.id)}>COMPLETED</button>
-            </li>
-          );
-        })}
-      </ul>
+        {/* lists of the entered tasks */}
+
+        <div className="flex flex-col">
+          {tasks.map((task) => {
+            return (
+              <div
+                key={task.id}
+                className="flex mb-5 items-center justify-between border-2 border-black py-2 px-4 rounded-2xl "
+              >
+                <h2
+                  className={`${
+                    task.completed ? "line-through" : ""
+                  }  font-bold text-xl`}
+                >
+                  {task.value}
+                </h2>
+                <div className="flex ">
+                  <button
+                    onClick={() => deleteTask(task.id)}
+                    className="hover:scale-150 text-xl mr-5 delay-100"
+                  >
+                    <AiOutlineDelete />
+                  </button>
+                  <button
+                    onClick={() => completedTasks(task.id)}
+                    className="hover:scale-150 text-xl delay-100"
+                  >
+                    <AiOutlineCheck />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
