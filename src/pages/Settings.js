2d0f429233e13b460ab045c36ffeca087ae0c091
Utku Cengiz <cekicengiz01@gmail.com>
2023-07-12 14:59:00 +0300

    profile img update

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,11 @@
 import { signOut, updatePassword, onAuthStateChanged } from "firebase/auth";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
 import { doc, updateDoc } from "firebase/firestore";
 import { toast } from "react-hot-toast";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 
 function Settings(props) {
   const [newPassword, setNewPassword] = useState("");
@@ -58,18 +60,66 @@ function Settings(props) {
     });
   };
 
+  // photo change
+
+  const [changeUrl, setChangeURl] = useState(null);
+  const [loadingChange, setLoadingChange] = useState(false);
+  const generateRandomName = () => {
+    const randomName = uuidv4();
+    return randomName;
+  };
+
+  const imageRef = ref(storage, `users/${generateRandomName()}`);
+
+  const fetchImageUrl = async () => {
+    try {
+      const downloadURL = await getDownloadURL(imageRef);
+      setChangeURl(downloadURL);
+      const docRef = doc(db, "users", props.userData.id);
+      updateDoc(docRef, { img: downloadURL });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingChange(false);
+    }
+  };
+
+  const handleSubmitPhoto = (e) => {
+    e.preventDefault();
+    const file = e.currentTarget.files[0];
+    setLoadingChange(true);
+    uploadBytes(imageRef, file)
+      .then(() => fetchImageUrl())
+      .catch((error) => {
+        console.log(error);
+        setLoadingChange(false);
+      });
+  };
+
   return (
     <div
       className={`responsive flex justify-center  items-center min-h-screen ${
         props.theme ? "bg-slate-900" : "bg-cyan-100"
       }`}
     >
-      <div className="flex absolute top-0 sm:right-0 p-5 text-white sm:text-2xl text-xl sm:gap-16 gap-8">
-        <span
-          className={` ${props.theme ? "text-slate-400" : "text-cyan-400"}`}
-        >
-          {props.userData?.username}
-        </span>
+      <div className="flex absolute top-0 items-center sm:right-0 p-5 text-white sm:text-2xl text-xl sm:gap-16 gap-8">
+        <div className="flex flex-row items-center sm:gap-4 gap-2">
+          <img
+            src={props.userData?.img}
+            alt=""
+            className="w-10 h-10 rounded-full"
+          />
+          <Link
+            to="/"
+            className={` text-center ${
+              props.theme
+                ? "text-slate-400 hover:text-slate-200 "
+                : "text-cyan-400 hover:text-cyan-600"
+            }`}
+          >
+            {props.userData?.username}
+          </Link>
+        </div>
         <Link
           to="/"
           className={` text-center ${
@@ -92,13 +142,13 @@ function Settings(props) {
         </button>
       </div>
       <div className="flex flex-col gap-5">
-        <form className="flex flex-row gap-4" onSubmit={handleSubmitUserName}>
+        <form className="flex flex-row gap-4  items-center" onSubmit={handleSubmitUserName}>
           <input
             type="text"
             placeholder="New Username"
             value={newUserName}
             onChange={(e) => setUserName(e.target.value)}
-            className={`border-2 rounded-2xl border-transparent shadow-lg py-2 px-4 text-xl font-bold outline-none w-full  ${
+            className={`border-2 rounded-2xl w-2/3 border-transparent shadow-lg py-2 px-4 text-xl font-bold outline-none  ${
               props.theme ? "bg-slate-700 text-white" : ""
             }`}
           />
@@ -113,13 +163,13 @@ function Settings(props) {
             Update
           </button>
         </form>
-        <form className="flex flex-row gap-4" onSubmit={handleSubmitPassword}>
+        <form className="flex flex-row gap-4  items-center" onSubmit={handleSubmitPassword}>
           <input
             type="password"
             placeholder="New Password"
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
-            className={`border-2 rounded-2xl border-transparent shadow-lg py-2 px-4 text-xl font-bold outline-none w-full  ${
+            className={`border-2 rounded-2xl w-2/3 border-transparent shadow-lg py-2 px-4 text-xl font-bold outline-none  ${
               props.theme ? "bg-slate-700 text-white" : ""
             }`}
           />
@@ -133,6 +183,49 @@ function Settings(props) {
           >
             Update
           </button>
+        </form>
+        <form className="flex flex-row gap-4  items-center">
+          <input
+            type="file"
+            onChange={handleSubmitPhoto}
+            className={`border-2 rounded-2xl w-2/3 border-transparent shadow-lg py-2 px-4 text-xl font-bold outline-none  ${
+              props.theme ? "bg-slate-700 text-white" : ""
+            }`}
+          />
+          {loadingChange ? (
+            <div
+              className={` text-right text-xl ${
+                props.theme
+                  ? "text-slate-400 hover:text-slate-200 "
+                  : "text-cyan-400 hover:text-cyan-600"
+              }`}
+            >
+              Loading...
+            </div>
+          ) : (
+            changeUrl && (
+              <div
+                className={` text-right text-xl ${
+                  props.theme
+                    ? "text-slate-400 hover:text-slate-200 "
+                    : "text-cyan-400 hover:text-cyan-600"
+                }`}
+              >
+                successful
+              </div>
+            )
+          )}
+
+          {/* <button
+            type="submit"
+            className={` text-right text-xl ${
+              props.theme
+                ? "text-slate-400 hover:text-slate-200 "
+                : "text-cyan-400 hover:text-cyan-600"
+            }`}
+          >
+            Update
+          </button> */}
         </form>
       </div>
     </div>
